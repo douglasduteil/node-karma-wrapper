7eeb6485c512bc7f81f36b80881a02db7d5fafc3
Douglas Duteil <douglasduteil@gmail.com>
2013-12-08 23:14:15 +0100

    fix(travis): make it travis safe.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,18 @@
 ;(function() {
   'use strict';
 
+  var assign = require('lodash.assign');
   var gulp = require('gulp');
   var karma = require('./index.js');
 
   gulp.task('default', function(cb){
-    karma({ configFile : './test/karma.conf.js'}, cb);
+    function testConfig(configFile, customOptions){
+      var options = { configFile: configFile, singleRun: true };
+      var travisOptions = process.env.TRAVIS && { browsers: [ 'Firefox', 'PhantomJS'], reporters: ['dots'] };
+      return assign(options, customOptions, travisOptions);
+    }
+
+    karma(testConfig('./test/karma.conf.js'), cb);
   });
 
 })();
