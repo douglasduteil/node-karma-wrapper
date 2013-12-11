20294b2b8d54c02f8b5fa62ee8e8cb08f88a87da
Douglas Duteil <douglasduteil@gmail.com>
2013-12-11 18:15:06 +0100

    refacto handy way

--- index.js
+++ index.js
@@ -6,36 +6,47 @@ var runner = require('karma').runner;
 var path = require('path');
 var spawn = require('child_process').spawn;
 
-function karmaPlugin(opts, done){
+function karmaPlugin(opts){
+  var _this = this || {};
 
   if( opts && opts.configFile){
     opts.configFile = path.join(process.cwd(), opts.configFile);
   }
 
-  var args = [path.join(__dirname , 'lib', 'server_process.js'), JSON.stringify(opts)];
-  if( opts && ! opts.background){
+  _this.config = opts;
+
+  _this.start = function (done) {
+    var args = [path.join(__dirname , 'lib', 'server_process.js'), JSON.stringify(_this.config)];
     spawn('node', args, { stdio: 'inherit' })
       .on('close', function (code) {
-        done(code);
+        done && done(code);
       });
-  }else{
+  };
+
+  _this.inBackground = function (done) {
+    var args = [path.join(__dirname , 'lib', 'server_process.js'), JSON.stringify(_this.config)];
     spawn('node', args)
       .on('close', function (code) {
         console.log('The background karma server stops. code ' + code);
       });
-    done();
-  }
+    done && done();
+  };
 
-}
+  _this.simpleRun = function (done) {
+    _this.config.singleRun = true;
+    console.log(_this.config);
+    var args = [path.join(__dirname , 'lib', 'server_process.js'), JSON.stringify(_this.config)];
+    spawn('node', args, { stdio: 'inherit' })
+      .on('close', function (code) {
+        done && done(code);
+      });
+  };
 
-karmaPlugin.run = function(opts, done){
+  _this.run = function (done) {
+    runner.run(_this.config, function() { done && done(); });
+  };
 
-  if( opts && opts.configFile){
-    opts.configFile = path.join(process.cwd(), opts.configFile);
-  }
-
-  runner.run(opts, function() { done(); });
-
+  return _this;
 };
 
-module.exports = karmaPlugin;
+module.exports =  karmaPlugin;
